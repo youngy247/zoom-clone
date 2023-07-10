a71b0809d7b0c35874ca0b2a90089c2f70d4eb30
Adam Young <adamyoungy678@gmail.com>
2023-07-10 17:00:24 +0100

    progress: sending your video stream to other in the same call

--- public/script.js
+++ public/script.js
@@ -12,15 +12,26 @@ navigator.mediaDevices.getUserMedia({
     audio: true
 }).then(stream => {
     addVideoStream(myVideo, stream)
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+    })
 })
 
 myPeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id)
 })
 
-socket.on('user-connected', userId => {
-    console.log('User connected' + userId)
-})
+function connectToNewUser(userId, stream){
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+}
 
 function addVideoStream(video, stream) {
     video.srcObject = stream
