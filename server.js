6e5f1d22d2d33a6accbb0f722b079bab0c8df9e1
Adam Young <adamyoungy678@gmail.com>
2023-07-10 16:46:32 +0100

    feat: established peer server connection

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.get('/:room', (req, res) => {
 
 io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
-        console.log(roomId, userId)
+        socket.join(roomId)
+        socket.broadcast.to(roomId).emit('user-connected', userId)
     })
 })
 
