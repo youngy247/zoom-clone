1f016bc38f1f49f879b93af16f8a25c49fc37af3
Adam Young <adamyoungy678@gmail.com>
2023-07-10 16:50:36 +0100

    feat: created self video and muted own microphone noise

--- public/script.js
+++ public/script.js
@@ -1,10 +1,15 @@
 const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     host: '/',
     port: '3001'
 })
+const myVideo = document.createElement('video')
+myVideo.muted = true
 
-socket.emit('join-room', ROOM_ID, 10)
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id)
+})
 
 socket.on('user-connected', userId => {
     console.log('User connected' + userId)
