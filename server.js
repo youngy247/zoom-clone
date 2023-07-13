7d713fc29c6952f0b1af004c47aa824e6173763d
Adam Young <adamyoungy678@gmail.com>
2023-07-13 12:48:32 +0100

    feat: added JWT token authentication admin route and created isActive

--- server.js
+++ server.js
@@ -6,15 +6,25 @@ const { v4: uuidV4 } = require('uuid');
 const connection = require('./db');
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto')
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
+
+const secretKey = process.env.SECRET_KEY;
 
 
 
 let isAvailable = true;
 let adminSocketId = null;
 let roomOccupied = false;
+let isActive = true;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(cors());
+app.use(cookieParser());
+
 
 // Add the body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -54,13 +64,45 @@ app.get('/login', (req, res) => {
   
         // Authentication successful
         // Set a session or generate a JWT token here for further authentication/authorization
-        res.send('Login successful');
+        const token = jwt.sign({ username: admin.username }, secretKey, { expiresIn: '3h' });
+        
+         // Send the token as the response
+        res.status(200).json({ token });
       });
     });
   });
 
+  // Middleware function to verify and authenticate the JWT token
+function authenticateToken(req, res, next) {
+
+    const token = req.cookies.token;
+    console.log('Token:', token); // Check the value of the token
+  
+    if (!token) {
+      return res.status(401).send('Unauthorized');
+    }
+  
+    jwt.verify(token, secretKey, (err, decoded) => {
+      if (err) {
+        return res.status(403).send('Invalid token');
+      }
+  
+      req.user = decoded;
+      next();
+    });
+  }
+  
+
+  // Protected admin route
+app.get('/admin', authenticateToken, (req, res) => {
+    // The token is valid and the user is authenticated
+    // Access the user information from req.user (decoded JWT payload)
+    res.render('admin', { username: req.user.username });
+  });
+  
+
 app.get('/', (req, res) => {
-  res.render('home', { isAvailable });
+  res.render('home', { isAvailable, isActive: isActive});
 });
 
 app.get('/:room', (req, res) => {
@@ -95,6 +137,7 @@ io.on('connection', (socket) => {
   socket.on('availability-change', (availability) => {
     if (socket.id === adminSocketId) {
       isAvailable = availability;
+      isActive = availability;
       io.emit('availability-updated', isAvailable); // Notify all clients about the availability status change
     }
   });
@@ -103,6 +146,8 @@ io.on('connection', (socket) => {
     if (socket.id === adminSocketId && !roomOccupied && isAvailable) {
       roomOccupied = true;
       socket.emit('call-accepted');
+      isAvailable = false; // Set availability to false when accepting a call
+      io.emit('availability-updated', isAvailable); // Notify all clients about the availability status change
     }
   });
 
