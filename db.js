e35d97e22a01bc7a45f1f0c1d78bd2837e05eb96
Adam Young <adamyoungy678@gmail.com>
2023-07-13 10:05:40 +0100

    feat: added db connection

---
+++ db.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  };
+
+const connection = mysql.createConnection(dbConfig);
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+module.exports = connection;
